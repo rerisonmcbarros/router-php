f5bc96df44ec178bf1c4d876b3726e1796b9562e
Rerison Barros <rerisonmcb@gmail.com>
2023-01-30 15:55:46 -0300

    feat: adição de funcionalidade a classe 'Request'
     No ramo main
     Mudanças a serem submetidas:
    	modified:   Lib/Core/Request.php
    	modified:   index.php

--- Lib/Core/Request.php
+++ Lib/Core/Request.php
@@ -4,5 +4,53 @@ namespace Lib\Core;
 
 class Request{
 	
-	
+	private $httpMethod;
+	public  $get;
+	public  $post;
+	private $uri;
+
+	/**
+	 * O '$prefix' é o nome do diretório raiz do projeto
+	 **/
+	private $prefix;
+
+
+	public function __construct(string $prefix){
+
+		$this->prefix = $prefix;
+		$this->httpMethod = filter_input(INPUT_SERVER, "REQUEST_METHOD");
+		$this->get = filter_input_array(INPUT_GET);
+		$this->post = filter_input_array(INPUT_POST);
+		$this->uri = parse_url(filter_input(INPUT_SERVER, "REQUEST_URI"), PHP_URL_PATH);
+	}
+
+	public function httpMethod(){
+
+		return $this->httpMethod;
+	}
+
+	public function get(){
+
+		return $this->get;
+	}
+
+	public function post(){
+
+		return $this->post;
+	}
+
+	public function uri(){
+
+		$uri = $this->uri;
+
+		if(strpos($this->uri, $this->prefix) !== false){
+
+			$prefixLen = strlen($this->prefix);
+
+			$uri = substr(strstr($this->uri, $this->prefix), $prefixLen);
+		}
+
+		return $uri;
+	}
+
 }
--- index.php
+++ index.php
@@ -3,26 +3,16 @@
 // Fazendo a inclusão do autoload ao arquivo raiz da aplicação.
 require_once __DIR__."/autoload.php";
 
-/* 
-** As Classes devem ser instanciadas fazendo uso dos seus namespaces
-** Caso a classe não seja encontrada será lançado um erro!
-*/
-
-/* 
-** exemplo 1 **
-
-use \Lib\Core\Route;
-
-$route = new Route();
-
-*/
-
-/* exemplo 2 */
+/**
+ * O nome do diretório raiz do projeto deve er passado como parâmetro no construtor da classe
+ * ex: new \Lib\Core\Request("Framework");
+ **/
+$request = new \Lib\Core\Request("Framework");
 
 $route = new \Lib\Core\Route();
 
 
-
+echo "<pre>", var_dump( $request->uri()), "</pre>";
 
 
 
